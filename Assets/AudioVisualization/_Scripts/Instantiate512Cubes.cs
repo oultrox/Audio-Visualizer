88d01fb972633a52733c35f9ff76c60121c9abe1
Jesús Pérez <jesus-perez95@hotmail.com>
2018-07-25 16:17:18 -0400

    [Upload] main project

---
+++ Assets/AudioVisualization/_Scripts/Instantiate512Cubes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate512Cubes : MonoBehaviour {
+
+    [SerializeField] private GameObject sampleCubePrefab;
+    [SerializeField] private float maxScale = 10;
+    private GameObject[] cubes = new GameObject[512];
+    // Use this for initialization
+	void Start () {
+        for (int i = 0; i < 512; i++)
+        {
+            GameObject instanceSampleCube = Instantiate(sampleCubePrefab);
+            instanceSampleCube.transform.position = this.transform.position;
+            instanceSampleCube.transform.parent = this.transform;
+            instanceSampleCube.name = "SampleCube " + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            instanceSampleCube.transform.position = Vector3.forward * 100f;
+            cubes[i] = instanceSampleCube;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < 512; i++)
+        {
+            if (cubes != null)
+            {
+                cubes[i].transform.localScale = new Vector3(10, (AudioPeer.samples[i] * maxScale) + 2, 10);
+            }
+        }
+	}
+}
---
+++ Assets/AudioVisualization/_Scripts/ParamCube.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour {
+
+    public int band;
+    public float startScale, scaleMultiplier;
+    public bool isUsingBuffer;
+    public bool isUsingColorChanger;
+    Material material;
+
+    private void Start()
+    {
+        material = GetComponent<MeshRenderer>().materials[0];
+    }
+
+    void Update ()
+    {
+        if (isUsingBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioPeer.audiobandBuffer[band] * scaleMultiplier) + startScale, transform.localScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioPeer.audioband[band] * scaleMultiplier) + startScale, transform.localScale.z);
+        }
+
+        if (isUsingColorChanger)
+        {
+            Color color = new Color(AudioPeer.audiobandBuffer[band], AudioPeer.audiobandBuffer[band], AudioPeer.audiobandBuffer[band]);
+            material.SetColor("_EmissionColor", color);
+        }
+    }
+}
